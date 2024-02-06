5575996d5d30d7196a8e3b042cc96413c72123f1
Shane <144045648+ShanesProgram@users.noreply.github.com>
2024-02-06 22:23:22 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+# Simple Number Game
+## Description
+
+The game starts with letting the player pick a number (1-9), then the computer picks a number (1-9 as well). If the numbers match, the player wins, if the numbers don't, the player can try again.
+
+## Installation
+
+### Windows
+
+1. Clone Repository 
+    - Go to the github page for this project
+    - Click the green button labelled "code"
+    - Go under the Local Tab
+    - Click "Download Zip"
+    - Once downloaded, unzip the folder
+2. Go into the repositories directory on your device
+3. Run the program "simpleNumberGameWin.exe"
+
+### Linux (Ubuntu)
+
+1. Ensure that Git is installed
+```
+sudo apt update
+sudo apt install git
+```
+2. Clone the repository
+```
+git clone ---
+```
+3. Run the binary file
+```
+cd ---
+./simpleNumberGameLin
+```
+
+### Linux (Other)
+I don't have instructions for other linux distros but if you are using a distro that isn't Ubuntu, you are probably smart enough to figure it out.
+
+## Building From Source
+
+
+## Linux
+
+1. Clone the repository
+
+cd build
+    -cmake -G "Unix Makefiles" ../Linux
+    -make
+    -run by doing the following command:
+        -./
---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+unsigned int number;        //number is the variable that holds the user's guess 
+unsigned int randomNumber;  //randomNumber is the variable that contains the randomNumber
+
+
+
+static void guessNumber(){
+    
+    std::cout << "Please make your number guess (1-9)" << std::endl;
+    std::cin >> number;
+
+    
+    if (number > 9){
+        std::cout << "Number is over 9, please try again" << std::endl;
+        guessNumber();
+    }
+    else if (number < 1){
+        std::cout << "Number is under 9, please try again" << std::endl;
+        guessNumber();
+    }
+    else {
+        return;
+    }
+}
+
+
+static void initializeRand(){
+    //Sets the seed of the random number generator to the time
+    srand(time(NULL));
+
+    int i = 0;
+    //Creates a loop that only ends if 'i' is between 1 to 10
+    while (i > 9 || i < 1){
+        //Createsthe random number
+        randomNumber = rand() % 9 + 1;
+        i = randomNumber;
+    }
+    std::cout << "Computers number is:" << randomNumber << std::endl;
+}
+
+static void logic(){
+    if (number == randomNumber){
+        std::cout << "You win!" << std::endl;
+    }
+    else if (number != randomNumber){
+        std::cout << "You Lost!" << std::endl;
+    }
+
+    //says if you press a key 
+    char nextGame;
+
+    std::cout << "(e)xit or (c)ontinue:";
+    std::cin >> nextGame;
+    std::cout << "" << std::endl;
+
+    if (nextGame == 'e'){
+        exit(1);
+    }
+    else if (nextGame == 'c'){
+        system("clear");
+    }
+    else {
+
+    }
+}
+
+void mainLoop(){
+    int i = 0;
+    while (i == 0){
+        guessNumber();
+        initializeRand();
+        logic();
+    }
+}
+
+int main(){
+    system("clear");
+    mainLoop();
+}
